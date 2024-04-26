77b12f50c0c19274ff96e18fc5c75656cbabb04f
petrovi4 <40.10.444@gmail.com>
2024-04-26 08:22:11 +0300

    version for review 1.0

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index, contact
+from catalog.apps import CatalogConfig
+from catalog.views import index, contact, get_product
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contact)
+    path('', index, name='index'),
+    path('contacts/', contact, name='contact'),
+    path('product/<pk>', get_product)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -5,11 +5,12 @@ from catalog.models import Product
 
 def index(request):
     context = {
-        'title': 'skystore',
+        'title': 'Главная',
         'products': Product.objects.all().order_by('-id')[:5]
+
     }
     print(context['products'])
-    return render(request, 'catalog/home.html', context=context)
+    return render(request, 'catalog/index.html', context=context)
 
 
 def contact(request):
@@ -18,4 +19,17 @@ def contact(request):
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'Новое сообщение от пользователя {name}({email}): {message}')
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+def get_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'title': product.product_name,
+        'product': product
+    }
+    print(context['product'])
+    return render(request, f'catalog/product.html', context=context)
